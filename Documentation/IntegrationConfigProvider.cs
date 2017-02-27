7d7e39c858736d088d54402e3d2d47b4f6f75a26
Queue-it <mala+github@queue-it.net>
2017-02-27 15:15:59 +0100

    Add files via upload

---
+++ Documentation/IntegrationConfigProvider.cs
@@ -0,0 +1,139 @@
+﻿using QueueIT.KnownUserV3.SDK.IntegrationConfig;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Timers;
+using System.Web.Script.Serialization;
+
+namespace QueueIT.KnownUserV3.SDK.IntegrationConfigLoader
+{
+
+
+    internal class IntegrationConfigProvider
+    {
+        private static readonly Lazy<IntegrationConfigProvider> _instance = new Lazy<IntegrationConfigProvider>(
+            () => new IntegrationConfigProvider());
+
+        private int _downloadTimeoutMS = 4000;
+        private Timer _timer;
+        private readonly object _lockObject = new object();
+        CustomerIntegration _cachedIntegrationConfig;
+        private bool _isInitialized = false;
+        public CustomerIntegration GetCachedIntegrationConfig(string customerId)
+        {
+            if(!this._isInitialized)
+            {
+                this.CustomerId = customerId;
+                lock (_lockObject)
+                {
+                    if (!this._isInitialized)
+                    {
+                        this.RefreshCache(true);
+                        if (this.Exp != null)
+                            throw this.Exp;
+                        _timer = new Timer();
+                        _timer.Interval = _RefreshIntervalS * 1000;
+                        _timer.AutoReset = false;
+                        _timer.Elapsed += TimerElapsed;
+                        _timer.Start();
+                        this._isInitialized = true;
+                    }
+                }
+            }
+            return this._cachedIntegrationConfig;
+        }
+
+        internal static int _RefreshIntervalS = 5 * 60;
+        internal static double _RetryExceptionSleepS = 5;
+        public Exception Exp { get; set; }
+        private string CustomerId { get; set; }
+
+        private IntegrationConfigProvider()
+        {
+        }
+        public static IntegrationConfigProvider Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+
+
+        public void Init(string customerId)
+        {
+            this.CustomerId = customerId;
+            lock (_lockObject)
+            {
+                if (_timer == null)
+                {
+                    this.RefreshCache(true);
+                    if (this.Exp != null)
+                        throw this.Exp;
+                    _timer = new Timer();
+                    _timer.Interval = _RefreshIntervalS * 1000;
+                    _timer.AutoReset = false;
+                    _timer.Elapsed += TimerElapsed;
+                    _timer.Start();
+                }
+            }
+        }
+
+
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            this.RefreshCache(false);
+            _timer.Start();
+        }
+
+        private void RefreshCache(bool init)
+        {
+            int tryCount = 0;
+            while (tryCount < 5)
+            {
+                var configUrl = $"https://assets.queue-it.net/{this.CustomerId}/integrationconfig/json/integrationInfo.json";
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(configUrl);
+                    request.Timeout = _downloadTimeoutMS;
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    {
+                        if (response.StatusCode != HttpStatusCode.OK)
+                            throw new Exception($"It was not sucessful retriving config file status code {response.StatusCode} from {configUrl}");
+                        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            //this.CachedIntegrationConfig = reader.ReadToEnd();
+                            JavaScriptSerializer deserializer = new JavaScriptSerializer();
+                            var deserialized = deserializer.Deserialize<CustomerIntegration>(reader.ReadToEnd());
+                            if (deserialized == null)
+                                throw new Exception("CustomerIntegration is null");
+                             _cachedIntegrationConfig = deserialized;
+
+
+                        }
+                        this.Exp = null;
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ++tryCount;
+                    if (tryCount >= 5)
+                    {
+                        this.Exp = new Exception($"Error in loading config file at {DateTime.UtcNow.ToString("o")}", ex);
+                        break;
+                    }
+                    if (!init)
+                        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(_RetryExceptionSleepS));
+                    else
+                        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(0.200 * tryCount));
+
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Documentation/QueueITHelpers.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace QueueIT.KnownUserV3.SDK.Sample
+{
+    public class QueueParameterHelper
+    {
+        public const string QueueITTokenKey = "queueittoken";
+        public const string _TimeStampKey = "ts";
+        public const string _ExtendCookieKey = "ce";
+        public const string _CookieValidityMinuteKey = "cv";
+        public const string _RedirectIdentifierKey = "ri";
+        public const string _ValidationStateKey = "vs";
+        public const string _QueueIdKey = "q";
+        public const string _HashKey = "h";
+        public const string _CustomerIdKey = "c";
+        public const string _EventIdKey = "e";
+        public const string _RedirectTypeKey = "rt";
+        public const string _DoHandShake = "dh";
+        public const char _KeyValueSeparatorChar = ':';
+        public const char _KeyValueSeparatorGroupChar = '&';
+        public static QueueUrlParams ExtractQueueParams(string queueitToken)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(queueitToken))
+                    return null;
+                QueueUrlParams result = new QueueUrlParams()
+                {
+                    QueueITToken = HttpUtility.UrlDecode(queueitToken)
+
+                };
+                var parames = result.QueueITToken.Split(_KeyValueSeparatorGroupChar);
+                foreach (var paramKeyValue in parames)
+                {
+                    var keyValueArr = paramKeyValue.Split(_KeyValueSeparatorChar);
+
+                    switch (keyValueArr[0])
+                    {
+                        case _TimeStampKey:
+                            {
+                                result.TimeStampString = keyValueArr[1];
+                                result.TimeStamp = DateTimeHelper.GetUnixTimeStampAsDate(result.TimeStampString);
+                                break;
+                            }
+                        case _CookieValidityMinuteKey:
+                            result.CookieValidityMinute = int.Parse(keyValueArr[1]);
+                            break;
+                        case _CustomerIdKey:
+                            result.CustomerId = keyValueArr[1];
+                            break;
+                        case _EventIdKey:
+                            result.EventId = keyValueArr[1];
+                            break;
+                        case _ExtendCookieKey:
+                            result.ExtendCookie = bool.Parse(keyValueArr[1]);
+                            break;
+                        case _HashKey:
+                            result.HashCode = keyValueArr[1];
+                            break;
+                        case _QueueIdKey:
+                            result.QueueId = keyValueArr[1];
+                            break;
+                        case _RedirectIdentifierKey:
+                            result.RedirectIdentifier = keyValueArr[1];
+                            break;
+                        case _RedirectTypeKey:
+                            result.RedirectType = keyValueArr[1];
+                            break;
+                        case _ValidationStateKey:
+                            result.ValidationState = keyValueArr[1];
+                            break;
+                        case _DoHandShake:
+                            result.DoHandShake = bool.Parse(keyValueArr[1]);
+                            break;
+                    }
+                }
+
+                result.QueueITTokenWithoutHash =
+                    result.QueueITToken.Replace($"{_KeyValueSeparatorGroupChar}{_HashKey}{_KeyValueSeparatorChar}{result.HashCode}", "");
+                return result;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        public static string GetPureUrlWithoutQueueITToken(string url)
+        {
+            return Regex.Replace(url, @"([\?&])(" + QueueITTokenKey + "=[^&]*)", string.Empty, RegexOptions.IgnoreCase);
+        }
+
+
+    }
+    public static class DateTimeHelper
+    {
+        public static DateTime GetUnixTimeStampAsDate(string timeStampString)
+        {
+            long timestampSeconds = long.Parse(timeStampString);
+            DateTime date1970 = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            return date1970.AddSeconds(timestampSeconds);
+        }
+        public static long GetUnixTimeStampFromDate(DateTime time)
+        {
+            return (long)(time.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+        //public static string ToDateTimeUTCString(DateTime dateTime)
+        //{
+        //    return dateTime.ToUniversalTime().ToString("o");
+        //}
+        //public static DateTime GetDateTimeUTCFromString(string utcString)
+        //{
+        //    DateTime result;
+        //    if (DateTime.TryParse(utcString, null, System.Globalization.DateTimeStyles.RoundtripKind, out result))
+        //        return result;
+        //    return DateTime.MinValue;
+        //}
+
+    }
+    public static class HashHelper
+    {
+        public static string GenerateSHA256Hash(string secretKey, string stringToHash)
+        {
+            using (HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(secretKey)))
+            {
+                return HttpUtility.UrlEncode(HttpUtility.UrlEncode(hmac.ComputeHash(Encoding.UTF8.GetBytes(stringToHash))));
+            }
+        }
+    }
+
+
+    public class QueueUrlParams
+    {
+        public DateTime TimeStamp { get; set; }
+        public string TimeStampString { get; set; }
+        public string QueueId { get; set; }
+        public string EventId { get; set; }
+        public string CustomerId { get; set; }
+        public string HashCode { get; set; }
+        public bool? ExtendCookie { get; set; }
+        public int? CookieValidityMinute { get; set; }
+        public string RedirectIdentifier { get; set; }
+        public string ValidationState { get; set; }
+        public string RedirectType { get; set; }
+        public bool DoHandShake { get; set; }
+        public string QueueITToken { get; set; }
+        public string QueueITTokenWithoutHash { get; set; }
+
+    }
+}
